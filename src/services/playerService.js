a177ee9174f43f49ae13714210dad05de4b02a28
udiwan <diwan.umang@gmail.com>
2024-08-03 01:06:57 -0400

    Started work on player service to make proper requests to the backend

---
+++ src/services/playerService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/players`;
+
+export const getPlayers = async () => {
+    try {
+        const response = await axios.get(BASE_URL);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching players:', error);
+    }
+};
+
+export const getSinglePlayer = async (playerId) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/${playerId}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching player:', error);
+    }
+};
+
+export const createPlayer = async (playerData) => {
+    try {
+        const response = await axios.post(BASE_URL, playerData);
+        return response.data;
+    } catch (error) {
+        console.error('Error creating player:', error);
+    }
+};
+
