c4b0dcdc9f5e2d6b0131476aaaf36f4578f577bc
udiwan <diwan.umang@gmail.com>
2024-09-26 01:08:17 -0400

    Building update and delete functionality

---
+++ src/components/PlayerUpdate/PlayerUpdate.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export const PlayerUpdate = ({ selectedPlayer }) => {
+    const [formData, setFormData] = useState({
+        name: "",
+        age: "",
+        position: "",
+        height: "",
+        weight: "",
+        shoots: "",
+        birthplace: "",
+        drafted: "",
+    });
+
+    return (
+        <div>
+
+        </div>
+    )
+}
