80c6c9357e0de51ca4e13eced71ddf30b7a9cd7f
udiwan <diwan.umang@gmail.com>
2024-10-14 23:45:54 -0400

    Took a break due to sickness. Back to styling.

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,6 +1,8 @@
+import "./homepage.css"
+
 export const Homepage = () => {
     return (
-        <div>
+        <div id="banner">
             <h1>Welcome to the Official Website of the New York Pucksters</h1>
         </div>
     )
