8951692e9a89603a0234eae9050e16a61ab9460f
udiwan <diwan.umang@gmail.com>
2024-10-16 00:39:19 -0400

    Further styling.

--- src/components/Roster/Roster.jsx
+++ src/components/Roster/Roster.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./roster.css"
 
 export const Roster = ({ setSelectedPlayer, playerList }) => {
 
@@ -7,18 +8,18 @@ export const Roster = ({ setSelectedPlayer, playerList }) => {
     };
 
     return (
-        <>
-            <h1>New York Pucksters H.C. Current Roster</h1>
+        <div id="rosterBox">
+            <h1 id="rosterBanner">New York Pucksters H.C. Current Roster</h1>
             {playerList.map((player) => (
-                <div key={player._id}>
+                <div className="rosterNames" key={player._id}>
                     <Link to={`/players/${player._id}`} onClick={() => handleClick(player)}>
                         <h2>{player.name}</h2>
                     </Link>
                 </div>
             ))}
             <Link to={'/players/new'}>
-                <button>Add New Player</button>
+                <button id="newPlayer">Add New Player</button>
             </Link>
-        </>
+        </div>
     );
 };
